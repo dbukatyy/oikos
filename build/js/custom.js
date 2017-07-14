6dd98c1ec7d7d3f187a73e092270d048c0575fb7
dbukatyy <dbukatyy@mail.ru>
2017-07-14 18:02:32 +0300

    first page

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,3 +1,13 @@
-"use strict";
+'use strict';
 
-jQuery(document).ready(function () {});
+jQuery(document).ready(function () {
+
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		margin: 10,
+		dots: true,
+		items: 1,
+		autoplay: true,
+		animateOut: 'fadeOut'
+	});
+});
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,5 +1,14 @@
 jQuery(document).ready(function () {
 
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    dots:true,
+	    items:1,
+	    autoplay: true,
+	    animateOut: 'fadeOut'
+	})
+
 });
 
 
