8fcc2dca6de06f376014bf847fc995e610096fc4
dbukatyy <dbukatyy@mail.ru>
2017-07-19 17:57:08 +0300

    all

--- src/js/custom.js
+++ src/js/custom.js
@@ -29,6 +29,30 @@ jQuery(document).ready(function () {
 	    animateOut: 'fadeOut',
 	    nav: false
 	})
+
+	$('.reviews__list').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    dots: true,
+	    items:2,
+	    autoplay: false,
+	    animateOut: 'fadeOut',
+	    nav: false
+	})
+
+	baguetteBox.run('.jobs__list');
+
+
+	$(document).on('scroll', e => {
+		const scroll = $(e.target).scrollTop(),
+			  header = $('.header');
+
+	    (scroll > 200) ? header.addClass('min') : header.removeClass('min');
+
+	    $('.content').css('background-position', `50% ${scroll/40}%`);
+	 
+			
+	});
 	
 
 });
